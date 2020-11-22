e8d93bb841e72cee967497537785f717f9209007
Thu Ya Aung <thuyaaung.dev.tya@gmail.com>
2020-11-22 20:43:04 +0630

    change to element plus

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+const Home = () => import("../views/Home.vue");
+const Login = () => import("../views/auth/Login.vue");
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
   },
   {
     path: "/about",
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="login">
+        it is login!
+    </div>
+    <div>hello</div>
+    <el-button>Button</el-button>
+</template>
+
+<script>
+export default {
+
+};
+</script>
